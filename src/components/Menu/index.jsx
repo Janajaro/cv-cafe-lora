95b99113514383d6082294c4bbcafe02ed90b1e8
Jana Jarošová <Janamjaros@seznam.cz>
2024-04-27 14:29:09 +0200

    komponenta Layer

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,6 +1,5 @@
 import { Drink } from '../Drink';
 
-
 export const Menu = () => {
   return (
     <section id="menu-id" className="menu">
@@ -10,21 +9,21 @@ export const Menu = () => {
           Vyberte si z našeho interaktivního menu a nemusíte čekat na obsluhu
         </p>
         <Drink
-  id={0}
-  name="Romano"
-  ordered={false}
-  image="http://localhost:4000/assets/cups/romano.png"
-  layers={[
-    {
-      color: '#fbdf5b',
-      label: 'citrón',
-    },
-    {
-      color: '#613916',
-      label: 'espresso',
-    },
-  ]}
-/>
+          id={0}
+          name="Romano"
+          ordered={false}
+          image="http://localhost:4000/assets/cups/romano.png"
+          layers={[
+            {
+              color: '#fbdf5b',
+              label: 'citrón',
+            },
+            {
+              color: '#613916',
+              label: 'espresso',
+            },
+          ]}
+        />
         <div className="order-detail">
           <a href="/order.html">Detail objednávky</a>
         </div>
